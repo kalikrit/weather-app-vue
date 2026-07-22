ffbb2db39d4969d1bf4b757154b4e35949324656
k.kononenko <kali-krit@mail.ru>
2026-07-22 14:47:41 +0300

    chore: bump version to 0.1.3

--- src/version.ts
+++ src/version.ts
@@ -1,3 +1,3 @@
 // Автоматически генерируется из package.json
-export const VERSION = "0.1.2";
-export const APP_NAME = "weather-app-ts";
+export const VERSION = '0.1.3'
+export const APP_NAME = 'weather-app-ts'
